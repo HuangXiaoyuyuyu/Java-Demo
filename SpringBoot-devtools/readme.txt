a55b98a074b75a8ee3c7b9d1ba65ad12866608b6
huangxiaoyuyuyu <253757635@qq.com>
2018-08-28 23:33:29 +0800

    devtools

---
+++ SpringBoot-devtools/readme.txt
@@ -0,0 +1,2 @@
+这是一个SpringBoot的热部署Demo，主要采用devtools进行热部署
+如有疑问~欢迎留言~
---
+++ SpringBoot-devtools/spring-boot-devtools/src/main/java/SpringBootDevtools/controller/HelloController.java
@@ -0,0 +1,14 @@
+package SpringBootDevtools.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+
+}
