b6c6ff76d9ed77b9791e1ebb4610eaefb5a0981d
huangxiaoyuyuyu <253757635@qq.com>
2018-09-10 22:04:28 +0800

    mybatis_config

---
+++ MyBatis_02_Config/src/org/hxy/mybatis/bean/Employee.java
@@ -0,0 +1,54 @@
+package org.hxy.mybatis.bean;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("emp")
+public class Employee {
+
+    private Integer id;
+    private String lastName;
+    private String gender;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
